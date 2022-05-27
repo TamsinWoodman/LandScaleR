e6cb68d77114c43d5224d30ef19493d31ba14be0
Woodman <r01tw20@abdn.ac.uk>
2022-05-27 12:18:30 +0100

    Initial attempt at a coarse cell S4 class

--- R/landCoverClasses.R
+++ R/landCoverClasses.R
@@ -1,48 +1,57 @@
-#' S4 class to hold a land cover map with associated properties
-#'
-#' @slot LC_map Data frame containing a land cover map. First two columns must
-#'   contain x- and y-coordinates for the map. Each subsequent column contains
-#'   the area of a given land cover class per cell.
-#' @slot agg_LC_map For fine-scale maps only, contains a copy of `LC_map` that
-#'   has been aggregated into coarse-scale grid cells.
-#' @slot LC_classes Vector of land cover classes in the `LC_map` slot. Must be
-#'   the same as land cover class column names in the `LC_map` data frame.
-#' @slot grid_cell_area Gives the area of each grid cell in the `LC_map` data
-#'   frame where an equal-area projection is used.
-#' @slot resolution Vector with the x- and y-dimensions of each grid cell where
-#'   all grid cells have the same resolution. Should be provided in the format
-#'   `c(x, y)`.
-setClass("LCMap",
-         slots = c(LC_map = "data.frame",
-                   agg_LC_map = "data.frame",
-                   LC_classes = "character",
-                   cell_area = "numeric",
-                   cell_resolution = "numeric"),
-         prototype = c(LC_map = data.frame(),
-                       agg_LC_map = data.frame(),
-                       LC_classes = NA_character_,
-                       cell_area = NA_real_,
-                       cell_resolution = NA_real_))
 
-#' S4 class to hold parameters required for land cover allocation
-#'
-#' @slot LC_deltas `LCMap` object containing the LC deltas data frame and
-#'   associated information.
-#' @slot ref_map `LCMap` object containing a reference map data frame and
-#'   associated information.
-#' @slot transition_priorities Matrix with the transition priorities for land
-#'   cover allocation.
-#' @slot kernel_radius Value for radius of cells used to calculate kernel
-#'   densities for each land cover and grid cell.
-#' @slot intensification_ratio Value giving the ratio of land cover to be
-#'   allocated via intensification.
-setClass("LCAllocationParams",
-         slots = c(LC_deltas = "LCMap",
-                   ref_map = "LCMap",
-                   kernel_radius = "numeric",
-                   random_seed = "numeric"),
-         prototype = c(LC_deltas = new("LCMap"),
-                       ref_map = new("LCMap"),
-                       kernel_radius = NA_real_,
-                       random_seed = NA_real_))
+setClass("MidCoords",
+         slots = list(mid_x = "numeric",
+                      mid_y = "numeric"),
+         prototype = list(mid_x = NA_real_,
+                          mid_y = NA_real_))
 
+setClass("OuterCoords",
+         slots = list(min_x = "numeric",
+                      min_y = "numeric",
+                      max_x = "numeric",
+                      max_y = "numeric"),
+         prototype = list(min_x = NA_real_,
+                          min_y = NA_real_,
+                          max_x = NA_real_,
+                          max_y = NA_real_))
+
+setClass("NeighbourCells",
+         slots = list(top = "numeric",
+                      right = "numeric",
+                      bottom = "numeric",
+                      left = "numeric",
+                      top_right = "numeric",
+                      bottom_right = "numeric",
+                      bottom_left = "numeric",
+                      top_left = "numeric"),
+         prototype = list(top = NA_real_,
+                          right = NA_real_,
+                          bottom = NA_real_,
+                          left = NA_real_,
+                          top_right = NA_real_,
+                          bottom_right = NA_real_,
+                          bottom_left = NA_real_,
+                          top_left = NA_real_))
+
+setClass("CoarseCell",
+         slots = list(id = "numeric",
+                      mid_coords = "MidCoords",
+                      outer_coords = "OuterCoords",
+                      LC_deltas = "numeric",
+                      neighbour_cells = "NeighbourCells",
+                      cell_area = "numeric",
+                      ref_cells = "data.frame",
+                      sum_ref_cells = "numeric",
+                      ref_cells_area = "numeric"),
+         prototype = list(id = NA_real_,
+                          mid_coords = new("MidCoords"),
+                          outer_coords = new("OuterCoords"),
+                          LC_deltas = NA_real_,
+                          neighbour_cells = new("NeighbourCells"),
+                          cell_area = NA_real_,
+                          ref_cells = data.frame(),
+                          sum_ref_cells = NA_real_,
+                          ref_cells_area = NA_real_),
+         contains = c("MidCoords",
+                      "OuterCoords",
+                      "NeighbourCells"))
