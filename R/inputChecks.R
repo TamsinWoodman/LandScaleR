9d5a7e5eed287c9e143cafa498067d1b24d60c2e
Woodman <r01tw20@abdn.ac.uk>
2022-10-07 14:18:45 +0100

    Specify package for crs fn

--- R/inputChecks.R
+++ R/inputChecks.R
@@ -2,7 +2,7 @@
 inputMapChecks <- function(LC_deltas,
                            ref_map) {
 
-  if (!crs(LC_deltas) == crs(ref_map)) {
+  if (!terra::crs(LC_deltas) == terra::crs(ref_map)) {
     stop("Projections of land cover change and reference maps do not match")
   }
 
