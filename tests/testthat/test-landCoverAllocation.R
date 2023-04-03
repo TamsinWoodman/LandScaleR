17169c4676949c67454031e18c5790c28313d0f7
Woodman <r01tw20@abdn.ac.uk>
2023-04-03 14:28:57 +0100

    Add unit tests for getCellsForAllocation and getAllocation DF

--- tests/testthat/test-landCoverAllocation.R
+++ tests/testthat/test-landCoverAllocation.R
@@ -85,3 +85,83 @@ test_that("updateRefCells updates expected reference map cells", {
                terra::values(expected_ref_cells))
 })
 
+test_that("getCellsForAllocation returns expected cell numbers", {
+
+  ref_cells <- terra::rast(xmin = 0,
+                           xmax = 3,
+                           ymin = 0,
+                           ymax = 3,
+                           res = 1,
+                           nlyrs = 2,
+                           names = c("pas", "pri"))
+  terra::values(ref_cells[["pas"]]) <- c(12, 0, 3, 5, 3, 3, 2, 7, 1)
+  terra::values(ref_cells[["pri"]]) <- c(4, 16, 13, 11, 13, 13, 14, 9, 15)
+
+  LC_from_name <- "pas"
+
+  expected_cells_for_allocation <- c(1, 3, 4, 5, 6, 7, 8, 9)
+
+  actual_cells_for_allocation <- getCellsForAllocation(ref_map = ref_cells,
+                                                       LC_from_name = LC_from_name)
+
+  expect_equal(actual_cells_for_allocation,
+               expected_cells_for_allocation)
+})
+
+test_that("getCellsForAllocation returns zero length integer vector if no cells contain the LC to be converted", {
+
+  ref_cells <- terra::rast(xmin = 0,
+                           xmax = 3,
+                           ymin = 0,
+                           ymax = 3,
+                           res = 1,
+                           nlyrs = 2,
+                           names = c("pas", "pri"))
+  terra::values(ref_cells[["pas"]]) <- rep(0, 9)
+  terra::values(ref_cells[["pri"]]) <- c(4, 16, 13, 11, 13, 13, 14, 9, 15)
+
+  LC_from_name <- "pas"
+
+  expected_cells_for_allocation <- integer(0)
+
+  actual_cells_for_allocation <- getCellsForAllocation(ref_map = ref_cells,
+                                                       LC_from_name = LC_from_name)
+
+  expect_equal(actual_cells_for_allocation,
+               expected_cells_for_allocation)
+})
+
+test_that("getAllocationDF returns expected data frame", {
+
+  ref_cells <- terra::rast(xmin = 0,
+                           xmax = 3,
+                           ymin = 0,
+                           ymax = 3,
+                           res = 1,
+                           nlyrs = 2,
+                           names = c("pas", "pri"))
+  terra::values(ref_cells[["pas"]]) <- c(12, 0, 0, 0, 3, 3, 2, 7, 1)
+  terra::values(ref_cells[["pri"]]) <- c(4, 16, 13, 11, 13, 13, 14, 9, 15)
+
+  kernel_densities <- ref_cells
+  terra::values(kernel_densities[["pas"]]) <- c(0.1, 1, 0.3, 0.5, 1.1, 1.2, 0.9, 0.1, 0.1)
+  terra::values(kernel_densities[["pri"]]) <- c(0.4, 1.3, 1.2, 0.7, 0.5, 0.2, 0.3, 0.9, 1.5)
+
+  LC_from_name <- "pas"
+  LC_to_name <- "pri"
+  cell_numbers_for_allocation <- c(1, 5, 6, 7, 8, 9)
+
+  expected_allocation_df <- data.frame(cell_number = cell_numbers_for_allocation,
+                                       pas = c(12, 3, 3, 2, 7, 1),
+                                       kernel_density = c(0.4, 0.5, 0.2, 0.3, 0.9, 1.5),
+                                       actual_conversion = 0)
+
+  actual_allocation_df <- getAllocationDF(cell_numbers_for_allocation = cell_numbers_for_allocation,
+                                          updated_ref_cells = ref_cells,
+                                          LC_from_name = LC_from_name,
+                                          LC_to_name = LC_to_name,
+                                          kernel_densities = kernel_densities)
+
+  expect_equal(actual_allocation_df,
+               expected_allocation_df)
+})
