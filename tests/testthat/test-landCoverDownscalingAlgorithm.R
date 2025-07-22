3994508ed1e4dc3b37c458a68abe34cfa727404d
Tamsin <t.woodman.20@abdn.ac.uk>
2025-07-22 16:35:52 +0100

    Add test for assignRefMapCells if some coarse resolution cells do not overlay fine resolution cells

--- tests/testthat/test-landCoverDownscalingAlgorithm.R
+++ tests/testthat/test-landCoverDownscalingAlgorithm.R
@@ -52,7 +52,7 @@ test_that("assignRefMap returns polygons with same CRS as reference map", {
   ref_map <- terra::rast(ref_map_crds,
                          type = "xyz",
                          crs = "EPSG:4326")
-
+  
   # Set up LC deltas data
   LC_deltas_crds <- matrix(data = c(0.5, 1,
                                     1.5, 1),
@@ -60,17 +60,51 @@ test_that("assignRefMap returns polygons with same CRS as reference map", {
                            ncol = 2,
                            byrow = TRUE)
   colnames(LC_deltas_crds) <- c("x", "y")
-
+  
   LC_deltas_cell_numbers <- 1:2
-
+  
   # Get actual output
   actual_polygons <- assignRefMapCells(ref_map = ref_map,
                                        LC_deltas_coords = LC_deltas_crds,
                                        LC_deltas_cell_numbers = LC_deltas_cell_numbers)
-
+  
   expect_equal(terra::crs(actual_polygons),
                terra::crs(ref_map))
 
+})
+
+test_that("assignRefMap returns expected polygons if coarse cells don't overlay all reference map cells", {
+  
+  # Set up ref map data
+  ref_map_crds <- data.frame(x = c(0.5, 0.5, 1.5, 1.5),
+                             y = c(0.5, 1.5, 0.5, 1.5),
+                             coarse_ID = c(1, 1, 1, 1))
+  ref_map <- terra::rast(ref_map_crds,
+                         type = "xyz",
+                         crs = "EPSG:4326")
+  
+  # Set up LC deltas data
+  LC_deltas_crds <- matrix(data = c(1.5, 1, 
+                                    2.5, 1),
+                           nrow = 2,
+                           ncol = 2,
+                           byrow = TRUE)
+  colnames(LC_deltas_crds) <- c("x", "y")
+  
+  LC_deltas_cell_numbers <- 1:2
+  
+  # Get expected output
+  expected_polygons <- terra::as.polygons(ref_map)
+  names(expected_polygons) <- "coarse_ID"
+  
+  # Get actual output
+  actual_polygons <- assignRefMapCells(ref_map = ref_map,
+                                       LC_deltas_coords = LC_deltas_crds,
+                                       LC_deltas_cell_numbers = LC_deltas_cell_numbers)
+  
+  expect_true(terra::all.equal(actual_polygons,
+                               expected_polygons))
+  
 })
 
 test_that("setRefMapLevels returns sorted data frame with sequential integer values if reference map contains areas", {
